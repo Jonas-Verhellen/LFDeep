60420debf2fd3d04d37863637d8cba6c28917099
Constantin Bechkov <constb@eduroam-193-157-202-89.wlan.uio.no>
2022-09-30 14:27:24 +0200

    update

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -16,10 +16,11 @@ import torch
 import wget
 from torch import nn
 from torch import Tensor
-from torch.utils.data import Dataset, DataLoader, TensorDataset
+from torch.utils.data import Dataset, DataLoader, TensorDataset, random_split
 from torch.nn.utils.rnn import pad_sequence
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import OneHotEncoder
+from utils_neuron import parse_multiple_sim_experiment_files
 
 PATH = "./"
 
@@ -27,9 +28,15 @@ def data_preparation_newdataset(params, data_directory="/home/"):
     """
     DATASET X: Template
     """
-    dataset_train = pd.read_csv(data_directory + "train_features.csv")
-    dataset_validation = pd.read_csv(data_directory + "val_features.csv")
-    dataset_test = pd.read_csv(data_directory + "test_features.csv")
+    
+    file_list = [data_directory + file_str for file_str in os.listdir(data_directory)]
+    datasets = parse_multiple_sim_experiment_files(file_list)
+
+    data_split = [random_split(datasets[i].T, [params['train_split'], params['validation_split'],
+                                               params['test_split']]) for i in range(len(datasets))]
+    dataset_train = [dat[0] for dat in data_split]
+    dataset_validation = [dat[1] for dat in data_split]
+    dataset_test = [dat[2] for dat in data_split]
 
     train_loader = DataLoader(dataset_train, shuffle=params["shuffle"], batch_size=params["batch_size"])
     validation_loader = DataLoader(dataset_validation, shuffle=params["shuffle"], batch_size=params["batch_size"])
@@ -40,3 +47,15 @@ def data_preparation_newdataset(params, data_directory="/home/"):
     output_info = None
 
     return (train_loader, validation_loader, test_loader, num_features, num_tasks, output_info)
+
+#%%
+data_directory = '/Users/constb/Data/archive/Data_test_subthreshold/'
+file_list = [data_directory + file_str for file_str in os.listdir(data_directory)]
+datasets = parse_multiple_sim_experiment_files(file_list)
+
+X, y_spike, y_soma, y_DVT = datasets
+y_total = np.vstack([y_DVT,np.stack([np.float16(y_spike),np.float16(y_soma)])])
+
+
+np.save(data_directory+'input_spikes.npy',X)
+np.save(data_directory+'output.npy',y_total)
