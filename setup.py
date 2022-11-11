7aabbb90c634dc6a3bc4a952062cdb41cc601d46
Jonas Verhellen <32571076+Jonas-Verhellen@users.noreply.github.com>
2022-11-11 17:03:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='LFDeep',
     version='0.0.1',
-    description='Multi-Expert Deep learning of Votlage Traces and Spike Times In A Cortical Neuron',
+    description='Multi-Expert Deep learning of Voltage Traces and Spike Times In A Cortical Neuron',
     author='',
     author_email='',
     url='https://github.com/Jonas-Verhellen/LFDeep',
